25ca98d73071cd8a80cdbec3cee742f5fd10a96d
silendas <muhammadyasminnulhakim34@gmail.com>
2024-06-07 07:37:04 +0700

    (feat): delete KKA & BAP

--- src/main/java/com/cms/audit/api/FollowUp/repository/FollowUpRepository.java
+++ src/main/java/com/cms/audit/api/FollowUp/repository/FollowUpRepository.java
@@ -13,7 +13,7 @@ import com.cms.audit.api.FollowUp.models.FollowUp;
 @Repository
 public interface FollowUpRepository extends JpaRepository<FollowUp, Long> {
 
-    @Query(value = "SELECT c.report_number, c.code, EXTRACT(YEAR FROM c.created_at) as created_year FROM follow_up c WHERE c.user_id = :userId ORDER BY c.id DESC LIMIT 1;", nativeQuery = true)
+    @Query(value = "SELECT c.report_number, c.code, EXTRACT(YEAR FROM c.created_at) as created_year FROM follow_up c WHERE c.user_id = :userId AND c.is_delete = 0 ORDER BY c.id DESC LIMIT 1;", nativeQuery = true)
     Optional<NumberClarificationInterface> checkNumberFollowUp(@Param("userId") Long id);
 
     @Query(value = "SELECT c.* FROM follow_up c WHERE c.clarification_id = :id ;", nativeQuery = true)
