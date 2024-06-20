93c14d76e76a5890b2200dd732d9363584a86705
silendas <muhammadyasminnulhakim34@gmail.com>
2024-06-20 20:20:36 +0700

    (fix): dashboard

--- src/main/java/com/cms/audit/api/Dashboard/service/DashboardTotalService.java
+++ src/main/java/com/cms/audit/api/Dashboard/service/DashboardTotalService.java
@@ -30,11 +30,11 @@ import com.cms.audit.api.Management.User.models.User;
 
 @Service
 public class DashboardTotalService {
-    
+
     @Autowired
     private ClarificationDashboardRepo clarificationRepo;
 
-    @Autowired 
+    @Autowired
     private ScheduleDashboardRepo scheduleRepository;
 
     @Autowired
@@ -79,7 +79,8 @@ public class DashboardTotalService {
         return returnResponse(response);
     }
 
-    private Specification<Clarification> buildClarificationSpecification(Long year, Long month, User getUser, boolean isAll) {
+    private Specification<Clarification> buildClarificationSpecification(Long year, Long month, User getUser,
+            boolean isAll) {
         Specification<Clarification> clarificationSpec = Specification
                 .where(new SpecificationFIlter<Clarification>().createdAtYear(year))
                 .and(new SpecificationFIlter<Clarification>().isNotDeleted());
@@ -87,12 +88,14 @@ public class DashboardTotalService {
         if (month != null && month != 0) {
             DateCompareDTO monthSeparate = convertDateToRoman.getDateRange(month);
             clarificationSpec = clarificationSpec.and(
-                    new SpecificationFIlter<Clarification>().dateRange(monthSeparate.getDate1(), monthSeparate.getDate2()));
+                    new SpecificationFIlter<Clarification>().dateRange(monthSeparate.getDate1(),
+                            monthSeparate.getDate2()));
         }
 
-        if(!isAll){
+        if (!isAll) {
             if (getUser.getLevel().getCode().equals("C")) {
-                clarificationSpec = clarificationSpec.and(new SpecificationFIlter<Clarification>().userId(getUser.getId()));
+                clarificationSpec = clarificationSpec
+                        .and(new SpecificationFIlter<Clarification>().userId(getUser.getId()));
             } else if (getUser.getLevel().getCode().equals("B")) {
                 Specification<Clarification> regionOrUserSpecClarification = Specification
                         .where(new SpecificationFIlter<Clarification>().getByRegionIds(getUser.getRegionId()))
@@ -115,7 +118,7 @@ public class DashboardTotalService {
                     new SpecificationFIlter<FollowUp>().dateRange(monthSeparate.getDate1(), monthSeparate.getDate2()));
         }
 
-        if(!isAll){
+        if (!isAll) {
             if (getUser.getLevel().getCode().equals("C")) {
                 followUpSpec = followUpSpec.and(new SpecificationFIlter<FollowUp>().userId(getUser.getId()));
             } else if (getUser.getLevel().getCode().equals("B")) {
@@ -140,7 +143,7 @@ public class DashboardTotalService {
                     new SpecificationFIlter<Schedule>().dateRange(monthSeparate.getDate1(), monthSeparate.getDate2()));
         }
 
-        if(!isAll) {
+        if (!isAll) {
             if (getUser.getLevel().getCode().equals("C")) {
                 scheduleSpec = scheduleSpec.and(new SpecificationFIlter<Schedule>().userId(getUser.getId()));
             } else if (getUser.getLevel().getCode().equals("B")) {
@@ -156,18 +159,16 @@ public class DashboardTotalService {
 
     private Map<String, Object> prepareClarificationData(List<Clarification> clarifications) {
         Map<String, Object> clarificationData = prepareData(
-            clarifications, 
-            c -> EStatusClarification.DONE.equals(c.getStatus())
-        );
+                clarifications,
+                c -> EStatusClarification.DONE.equals(c.getStatus()));
 
         return clarificationData;
     }
 
     private Map<String, Object> prepareFollowUpData(List<FollowUp> followUps) {
         Map<String, Object> followUpData = prepareData(
-            followUps, 
-            f -> EStatusFollowup.CLOSE.equals(f.getStatus())
-        );
+                followUps,
+                f -> EStatusFollowup.CLOSE.equals(f.getStatus()));
 
         return followUpData;
     }
@@ -204,7 +205,7 @@ public class DashboardTotalService {
         Map<String, Object> data = new HashMap<>();
         data.put("total", total);
         data.put("done", done);
-        
+
         return data;
     }
 
@@ -220,39 +221,41 @@ public class DashboardTotalService {
                 }
             }
         }
-    
+
         List<Map.Entry<User, Long>> sortedEntries = userClarificationCount.entrySet().stream()
                 .sorted((e1, e2) -> {
                     int comparison = Long.compare(e2.getValue(), e1.getValue()); // Descending by total
                     if (comparison == 0) {
-                        return Long.compare(userDoneClarificationCount.getOrDefault(e2.getKey(), 0L), userDoneClarificationCount.getOrDefault(e1.getKey(), 0L)); // Descending by done
+                        return Long.compare(userDoneClarificationCount.getOrDefault(e2.getKey(), 0L),
+                                userDoneClarificationCount.getOrDefault(e1.getKey(), 0L)); // Descending by done
                     }
                     return comparison;
                 })
                 .collect(Collectors.toList());
-    
+
         List<Map<String, Object>> result = new ArrayList<>();
         int count = 0;
         int maxResults = 5;
         for (Map.Entry<User, Long> entry : sortedEntries) {
-            if (count >= maxResults) break;
-    
+            if (count >= maxResults)
+                break;
+
             User user = entry.getKey();
             Long totalClarifications = entry.getValue();
             Long doneClarifications = userDoneClarificationCount.getOrDefault(user, 0L);
-    
+
             Map<String, Object> userData = new HashMap<>();
             userData.put("name", user.getFullname());
             userData.put("total", totalClarifications);
             userData.put("done", doneClarifications);
             result.add(userData);
-    
+
             count++;
         }
-    
+
         return result;
     }
-    
+
     private List<Map<String, Object>> prepareBottomData(List<Clarification> clarifications) {
         Map<User, Long> userClarificationCount = new HashMap<>();
         Map<User, Long> userDoneClarificationCount = new HashMap<>();
@@ -265,40 +268,43 @@ public class DashboardTotalService {
                 }
             }
         }
-    
+
         List<Map.Entry<User, Long>> sortedEntries = userClarificationCount.entrySet().stream()
                 .sorted((e1, e2) -> {
                     int comparison = Long.compare(e1.getValue(), e2.getValue()); // Ascending by total
                     if (comparison == 0) {
-                        return Long.compare(userDoneClarificationCount.getOrDefault(e1.getKey(), 0L), userDoneClarificationCount.getOrDefault(e2.getKey(), 0L)); // Ascending by done
+                        return Long.compare(userDoneClarificationCount.getOrDefault(e1.getKey(), 0L),
+                                userDoneClarificationCount.getOrDefault(e2.getKey(), 0L)); // Ascending by done
                     }
                     return comparison;
                 })
                 .collect(Collectors.toList());
-    
+
         List<Map<String, Object>> result = new ArrayList<>();
         int count = 0;
         int maxResults = 5;
         for (Map.Entry<User, Long> entry : sortedEntries) {
-            if (count >= maxResults) break;
-    
+            if (count >= maxResults)
+                break;
+
             User user = entry.getKey();
             Long totalClarifications = entry.getValue();
             Long doneClarifications = userDoneClarificationCount.getOrDefault(user, 0L);
-    
+
             Map<String, Object> userData = new HashMap<>();
             userData.put("name", user.getFullname());
             userData.put("total", totalClarifications);
             userData.put("done", doneClarifications);
             result.add(userData);
-    
+
             count++;
         }
-    
-        return result;
-    }    
 
-    private Map<String, Object> prepareTopBotResponse(List<Map<String, Object>> top5, List<Map<String, Object>> bottom5) {
+        return result;
+    }
+
+    private Map<String, Object> prepareTopBotResponse(List<Map<String, Object>> top5,
+            List<Map<String, Object>> bottom5) {
         Map<String, Object> topBotData = new HashMap<>();
         topBotData.put("top", top5);
         topBotData.put("bottom", bottom5);
@@ -311,12 +317,12 @@ public class DashboardTotalService {
                 .where(new SpecificationFIlter<User>().isNotDeleted())
                 .and(new SpecificationFIlter<User>().userLevelId(2L));
         List<User> listUser = userRepo.findAll(userSpec);
-    
+
         // Jika daftar pengguna kosong, kembalikan daftar kosong
         if (listUser.isEmpty()) {
             return new ArrayList<>();
         }
-    
+
         // Buat peta untuk menghitung jumlah tindak lanjut untuk setiap pengguna
         Map<User, Long> userFollowUpCount = new HashMap<>();
         for (FollowUp followUp : followUps) {
@@ -324,10 +330,15 @@ public class DashboardTotalService {
                 User user = userRepo.findById(followUp.getUpdated_by()).orElse(null);
                 if (user != null) {
                     userFollowUpCount.put(user, userFollowUpCount.getOrDefault(user, 0L) + 1);
+                } else {
+                    User createdBy = userRepo.findById(followUp.getCreated_by()).orElse(null);
+                    if(createdBy != null) {
+                        userFollowUpCount.put(createdBy, userFollowUpCount.getOrDefault(createdBy, 0L) + 1);
+                    }
                 }
             }
         }
-    
+
         // Buat daftar hasil
         List<Map<String, Object>> resultList = new ArrayList<>();
         for (User user : listUser) {
@@ -335,15 +346,16 @@ public class DashboardTotalService {
             Long closedFollowUps = followUps.stream()
                     .filter(f -> user.equals(f.getUser()) && EStatusFollowup.CLOSE.equals(f.getStatus()))
                     .count();
-    
+
             Map<String, Object> userData = new HashMap<>();
             userData.put("name", user.getFullname());
             userData.put("total", totalFollowUps);
             userData.put("done", closedFollowUps);
             resultList.add(userData);
         }
-    
-        // Urutkan hasil berdasarkan jumlah total tindak lanjut (descending), dan jika sama, berdasarkan jumlah done (descending)
+
+        // Urutkan hasil berdasarkan jumlah total tindak lanjut (descending), dan jika
+        // sama, berdasarkan jumlah done (descending)
         resultList.sort((u1, u2) -> {
             int comparison = Long.compare((Long) u2.get("total"), (Long) u1.get("total"));
             if (comparison == 0) {
@@ -351,15 +363,16 @@ public class DashboardTotalService {
             }
             return comparison;
         });
-    
+
         return resultList;
-    }    
+    }
 
     private Map<String, Object> prepareDashboardResponse(Long year, Long month, Map<String, Object> chartData,
             Map<String, Object> topBotData, List<Map<String, Object>> followUp) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("year", year != null ? year : convertDateToRoman.getLongYearNumber(new Date()));
-        response.put("month", month != null ? convertDateToRoman.getMonthName(month) : convertDateToRoman.getMonthName(convertDateToRoman.getLongMonthNumber(new Date())));
+        response.put("month", month != null ? convertDateToRoman.getMonthName(month)
+                : convertDateToRoman.getMonthName(convertDateToRoman.getLongMonthNumber(new Date())));
         response.put("summary", chartData);
 
         Map<String, Object> rankingMap = new LinkedHashMap<>();
